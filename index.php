b9e69af6af618d523addfae29a9f96f093c28ad3
dwnwp <125273095+dwnwp@users.noreply.github.com>
2024-03-22 19:02:33 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,6 +1,23 @@
 <?php
 session_start();
 include 'config.php';
+// product all
+$query = mysqli_query($conn, "SELECT * from products");
+$rows = mysqli_num_rows($query);
+
+// variable for product form
+$result = ['id' => '', 'product_name' => '', 'price' => '', 'product_image' => '', 'brand' => ''];
+
+// product select edit
+if (!empty($_GET['id'])) {
+    $query_product = mysqli_query($conn, "select * from products where id='{$_GET['id']}'");
+    $row_product = mysqli_num_rows($query_product);
+
+    if ($row_product == 0) {
+        header('location:' . $base_url . '/index.php');
+    }
+    $result = mysqli_fetch_assoc($query_product);
+}
 
 ?>
 
@@ -26,7 +43,7 @@ include 'config.php';
                 </a>
                 <ul class="nav col-12 col-lg-auto my-2 justify-content-center my-md-0 text-small">
                     <li>
-                        <a href="#" class="nav-link text-white">
+                        <a href="home.html" class="nav-link text-white">
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-house-door d-block mx-auto mb-1" viewBox="0 0 16 16">
                                 <path d="M8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4.5a.5.5 0 0 0 .5-.5v-4h2v4a.5.5 0 0 0 .5.5H14a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293zM2.5 14V7.707l5.5-5.5 5.5 5.5V14H10v-4a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5v4z" />
                             </svg>
@@ -34,7 +51,7 @@ include 'config.php';
                         </a>
                     </li>
                     <li>
-                        <a href="order.html" class="nav-link text-white">
+                        <a href="mama.php" class="nav-link text-white">
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-menu-button-wide d-block mx-auto mb-1" viewBox="0 0 16 16">
                                 <path d="M0 1.5A1.5 1.5 0 0 1 1.5 0h13A1.5 1.5 0 0 1 16 1.5v2A1.5 1.5 0 0 1 14.5 5h-13A1.5 1.5 0 0 1 0 3.5zM1.5 1a.5.5 0 0 0-.5.5v2a.5.5 0 0 0 .5.5h13a.5.5 0 0 0 .5-.5v-2a.5.5 0 0 0-.5-.5z" />
                                 <path d="M2 2.5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 0 1h-3a.5.5 0 0 1-.5-.5m10.823.323-.396-.396A.25.25 0 0 1 12.604 2h.792a.25.25 0 0 1 .177.427l-.396.396a.25.25 0 0 1-.354 0M0 8a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm1 3v2a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2zm14-1V8a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v2zM2 8.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5m0 4a.5.5 0 0 1 .5-.5h6a.5.5 0 0 1 0 1h-6a.5.5 0 0 1-.5-.5" />
@@ -65,34 +82,100 @@ include 'config.php';
     <!-- End Nav Bar -->
 
     <!-- Form Submit Alert -->
-    <?php if(!empty($_SESSION['message'])): ?>
+    <?php if (!empty($_SESSION['message'])) : ?>
         <div class="alert alert-warning alert-dismissible fade show" role="alert">
             <?php echo $_SESSION['message']; ?>
             <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
         </div>
-        <?php unset($_SESSION['message'])?>
-        <?php endif; ?>
+        <?php unset($_SESSION['message']) ?>
+    <?php endif; ?>
     <!-- End Form Submit Alert -->
 
     <!-- Form -->
     <form action="product-form.php" method="post" enctype="multipart/form-data">
+        <input type="hidden" name="id" value="<?php echo $result['id']; ?>">
         <h4 class="pb-2 m-5">Manage Product</h4>
+        <!-- product_name -->
         <label class="form-label">
-            Product Name: 
-            <input type="text" name="product_name" class="form-control" value="">
+            Product Name:
+            <input type="text" name="product_name" class="form-control" value="<?php echo $result['product_name']; ?>">
         </label>
+        <!-- price -->
         <label class="form-label">
-            Price: 
-            <input type="text" name="price" class="form-control" value="">
+            Price:
+            <input type="text" name="price" class="form-control" value="<?php echo $result['price']; ?>">
         </label>
+        <!-- profile_image -->
+        <?php if (!empty($result['profile_image'])) : ?>
+            <img src="<?php echo $base_url; ?>/img/<?php echo $result['profile_image']; ?>" width="100" alt="Product Image">
+        <?php endif; ?>
         <label for="formFile" class="form-label">
             Image:
             <input type="file" name="profile_image" class="form-control" accept="image/png, image/jpg, image/jpeg">
         </label>
-        <button class="btn btn-primary" type="submit">Create</button>
+        <!-- brand -->
+        <label class="form-label">
+            Brand:
+            <div class="dropdown">
+                <select name = "brand" class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    <option value="" selected hidden>Select Brand</option>
+                    <option value="mama">mama</option>
+                    <option value="waiwai">waiwai</option>
+                    <option value="samyang">samyang</option>
+                    <option value="yumyum">yumyum</option>
+                </select>
+            </div>
+        </label>
+        <!-- Submit -->
+        <?php if (empty($result['id'])) : ?>
+            <button class="btn btn-primary" type="submit">Create</button>
+        <?php else : ?>
+            <button class="btn btn-primary" type="submit">Update</button>
+        <?php endif; ?>
+        <a role="button" class="btn btn-secondary" href="index.php">Cancel</a>
         <hr class="my-4">
     </form>
     <!-- End Form -->
+
+    <!-- Table -->
+    <div class="container">
+        <table class="table table-border border-info">
+            <thead>
+                <tr>
+                    <th>Image</th>
+                    <th>Product Name</th>
+                    <th>Price</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php if ($rows > 0) : ?>
+                    <?php while ($product = mysqli_fetch_assoc($query)) : ?>
+                        <tr>
+                            <td>
+                                <?php if (!empty($product['profile_image'])) : ?>
+                                    <img src="<?php echo $base_url; ?>/img/<?php echo $product['profile_image']; ?>" width="100" alt="Product Image">
+                                <?php else : ?>
+                                    <img src="https://st3.depositphotos.com/23594922/31822/v/450/depositphotos_318221368-stock-illustration-missing-picture-page-for-website.jpg" width="100" alt="Product Image">
+                                <?php endif; ?>
+                            </td>
+                            <td>
+                                <?php echo $product['product_name']; ?>
+                            </td>
+                            <td>
+                                <?php echo number_format($product['price'], 2); ?>
+                            </td>
+                            <td>
+                                <a role="button" href="index.php?id=<?php echo $product['id']; ?>" class="btn btn-outline-dark">Edit</a>
+                                <a onclick="return confirm('Are you sure you want to delete?');" role="button" href="product-delete.php?id=<?php echo $product['id']; ?>" class="btn btn-outline-dark">Delete</a>
+                            </td>
+                        </tr>
+                    <?php endwhile; ?>
+                <?php endif; ?>
+            </tbody>
+        </table>
+    </div>
+    <!-- End Table -->
 
     <script src="js/bootstrap.min.js"></script>
 </body>
--- product-form.php
+++ product-form.php
@@ -3,14 +3,31 @@ session_start();
 include 'config.php';
 
 $product_name = trim($_POST['product_name']);
-$price = trim($_POST['price']);
+$price = $_POST['price'] ?: 0;
 $image_name = $_FILES['profile_image']['name'];
+$brand = $_POST['brand'];
 
 $image_tmp = $_FILES['profile_image']['tmp_name'];
 $folder = 'img/';
 $image_location = $folder . $image_name;
 
-$query = mysqli_query($conn, "INSERT INTO products(product_name, price, profile_image) VALUES('{$product_name}','{$price}','{$image_name}')") or die("Query Failed");
+// For create new product
+if (empty($_POST['id'])) {
+    $query = mysqli_query($conn, "INSERT INTO products(product_name, price, profile_image, brand) VALUES('{$product_name}','{$price}','{$image_name}','{$brand}');") or die("Query Failed");
+}
+ // For update product
+else {
+    $query_product = mysqli_query($conn, "select * from products where id='{$_POST['id']}'");
+    $result = mysqli_fetch_assoc($query_product);
+
+    if(empty($image_name)) {
+        $image_name = $result['profile_image'];
+    } else {
+        @unlink($folder . $result['profile_image']);
+    }
+    $query = mysqli_query($conn, "UPDATE products SET product_name='{$product_name}', price='{$price}', profile_image='{$image_name}', brand='{$brand}' WHERE id='{$_POST['id']}'") or die("Query Failed");
+}
+mysqli_close($conn);
 
 if ($query) {
     move_uploaded_file($image_tmp, $image_location);
